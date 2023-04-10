c49764a1fec078f001d3ac63b27cf0e7affb4281
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2023-04-10 15:06:21 +0300

    basic structure

---
+++ src/main/java/SearchEngineMain.java
@@ -0,0 +1,18 @@
+
+public class SearchEngineMain {
+	public static void main (String args[]) {
+		//run queries
+	}
+	
+	public static void parseDocuments() {
+		
+	}
+	
+	public static void createIndex() {
+		
+	}
+	
+	public static void search() {
+		
+	}
+}
