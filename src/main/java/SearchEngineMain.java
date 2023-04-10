08be2cd22cfe8dbee81af9ed59de318119eda965
tassos37000 <tassos37000@gmail.com>
2023-04-10 22:05:53 +0300

    implementation of parseDocuments

--- src/main/java/SearchEngineMain.java
+++ src/main/java/SearchEngineMain.java
@@ -1,11 +1,56 @@
+import java.io.*;
+import java.util.ArrayList;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StoredField;
+import org.apache.lucene.document.TextField;
 
 public class SearchEngineMain {
-	public static void main (String args[]) {
+	public static void main (String[] args) {
 		//run queries
+		try {
+			System.out.println(parseDocuments());
+		} catch (IOException e) {
+			System.out.println("Could not read the file");
+		}
 	}
 	
-	public static void parseDocuments() {
-		
+	public static ArrayList<Document> parseDocuments() throws IOException {
+		File file = new File("./collection/documents.txt");
+		BufferedReader br  = null;
+		try {
+			br = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		}
+
+		String st;
+		String id = "";
+		String text = "";
+		ArrayList<Document> documentList = new ArrayList<>();
+		while ((st = br.readLine()) != null){
+			try {  
+				Integer.parseInt(st);  
+				id = st;
+			} catch(NumberFormatException e){
+				if(st.equals(" /// ")){
+					Document doc = new Document();
+					TextField contents = new TextField("contents", text, Field.Store.NO);
+					StoredField docId = new StoredField("id", id);
+					doc.add(docId);
+					doc.add(contents);
+					documentList.add(doc);
+					text = "";
+				} 
+				else{ 
+					text += st;
+				}
+			}
+		}
+
+		br.close();
+		System.out.println("Parsing Done\n");
+		return documentList;
 	}
 	
 	public static void createIndex() {
