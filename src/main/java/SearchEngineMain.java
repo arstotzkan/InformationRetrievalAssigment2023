a6e0ef07d33e98cafc63baf003d158ffc1f35f4b
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2023-04-27 20:24:01 +0300

    added indexer method

--- src/main/java/SearchEngineMain.java
+++ src/main/java/SearchEngineMain.java
@@ -1,15 +1,29 @@
 import java.io.*;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Date;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StoredField;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.search.similarities.BM25Similarity;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 
 public class SearchEngineMain {
+
+	static String indexLocation = ("index");
 	public static void main (String[] args) {
 		//run queries
 		try {
-			System.out.println(parseDocuments());
+			ArrayList<Document> docList = parseDocuments();
+			createIndex(docList);
 		} catch (IOException e) {
 			System.out.println("Could not read the file");
 		}
@@ -53,11 +67,37 @@ public class SearchEngineMain {
 		return documentList;
 	}
 	
-	public static void createIndex() {
-		
+	public static void createIndex(ArrayList<Document> docs) {
+		Date start = new Date();
+
+		try{
+			Directory dir = FSDirectory.open(Paths.get(indexLocation));
+			Analyzer analyzer = new EnglishAnalyzer();
+			BM25Similarity similarity = new BM25Similarity();
+			IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+			iwc.setSimilarity(similarity);
+			iwc.setOpenMode(OpenMode.CREATE);
+
+			IndexWriter indexWriter = new IndexWriter(dir, iwc);
+
+			for (Document doc : docs){
+				System.out.println("adding " + doc.getField("id"));
+				indexWriter.addDocument(doc);
+			}
+
+			System.out.println(indexWriter);
+			indexWriter.close();
+
+			Date end = new Date();
+			System.out.println(end.getTime() - start.getTime() + " total milliseconds");
+
+		}
+		catch (IOException e){
+			System.out.println(" caught a " + e.getClass() +
+					"\n with message: " + e.getMessage());
+		}
 	}
-	
-	public static void search() {
-		
+
+	public static void search(String searchQuery) {
 	}
 }
