6ff123b84964ab1849b93af25121e2b8af4d0127
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2023-04-30 12:21:22 +0300

    Update run_trec_eval.bat

--- collection/run_trec_eval.bat
+++ collection/run_trec_eval.bat
@@ -1,4 +1,6 @@
-cd collection/ REM change dir into the dir where trec_eval exists
+cd collection/
+REM change dir into the dir where trec_eval exists
+
 trec_eval qrels.txt top50queryResults.txt
 echo Process Completed\nPress any button to exit...
 timeout /t -1
