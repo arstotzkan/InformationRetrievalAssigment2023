503fa8d516e58a03f56174b3bda9b1ed31c04e42
tassos37000 <tassos37000@gmail.com>
2023-04-30 12:27:43 +0300

    update batch file to run for top 20,30,50 texts

--- collection/run_trec_eval.bat
+++ collection/run_trec_eval.bat
@@ -1,6 +1,13 @@
 cd collection/
 REM change dir into the dir where trec_eval exists
 
+echo first 20 texts
+trec_eval qrels.txt top20queryResults.txt
+
+echo first 30 texts
+trec_eval qrels.txt top30queryResults.txt
+
+echo first 50 texts
 trec_eval qrels.txt top50queryResults.txt
 echo Process Completed\nPress any button to exit...
 timeout /t -1
